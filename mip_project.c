491f34adff07c4366aa702b2473ee917656d0912
nuke <nuke@nukes-mbp.dynamic.ucsd.edu>
2017-11-14 19:36:38 -0800

    somewhat fancy control loop, works

--- mip_project.c
+++ mip_project.c
@@ -15,6 +15,17 @@
 // function declarations
 void on_pause_pressed();
 void on_pause_released();
+int modeflag=0; //0=stay at reference postiion, 1= follow left wheel
+
+
+void print_usage(){
+	printf("\n");
+	printf("-f          Enable Wheel Follow Mode\n");
+	printf("-k {float}  Define proportional feedback Constant (between 0 and 1)\n");
+	printf("-s          Enable Stay at Reference Mode\n");  
+	printf("-h          print this help message\n");
+	printf("\n");
+}
 
 
 /*******************************************************************************
@@ -25,36 +36,162 @@ void on_pause_released();
 * - main while loop that checks for EXITING condition
 * - rc_cleanup() at the end
 *******************************************************************************/
-int main(){
+int main(int argc, char *argv[]){
+	
+	
+	//Define the 'polarity' of motors due to them being mirror images of each other
+	int rightpolarity=-1;
+	int leftpolarity=1;
+
+	//Define motor and encoder channels for left and right side.
+	int rightchannel=2;
+	int leftchannel=3;	
+	
+	float gearboxratio=4*15*35.577; //Define conversion factor of ticks per radian
+	float k=0.25; //Define proportional constant 
+
+	
+	
+	// parse arguments
+	opterr = 0;
+	int c;
+	float in;
+	while ((c = getopt(argc, argv, "k:fsh")) != -1){
+		switch (c){
+		case 'k': // custom proportional response specified
+			in = atof(optarg);
+			if(in>0 && in<2){
+				k = in;
+			}
+			else{
+				printf("k must be between 0 and 2.\n");
+				return -1;
+			}
+			break;
+		case 'f': //follow mode option
+			modeflag=1;
+			break;
+		case 's': //stay at ref option
+			modeflag=0;
+			break;
+		case 'h': //print help
+			print_usage();
+			return -1;
+			break;
+		default:
+			print_usage();
+			return -1;
+			break;
+		}
+		printf("%c", c);
+	}
+
+
 	// always initialize cape library first
 	if(rc_initialize()){
 		fprintf(stderr,"ERROR: failed to initialize rc_initialize(), are you root?\n");
 		return -1;
 	}
 
+
 	// do your own initialization here
-	printf("\nHello BeagleBone\n");
 	rc_set_pause_pressed_func(&on_pause_pressed);
 	rc_set_pause_released_func(&on_pause_released);
 
+	//Wake motors out of low power state
+	if(rc_enable_motors()){
+		fprintf(stderr,"ERROR: failed to enable motors\n");
+		return -1;
+	}
+
 	// done initializing so set state to RUNNING
 	rc_set_state(RUNNING); 
+	
+	//Print 'skelton' to display wheel positions
+	printf("\nEncoder positions (Radians)\n");
+	printf("   Right   |");
+	printf("   Left   |");
+	printf(" \n");
+
 
 	// Keep looping until state changes to EXITING
 	while(rc_get_state()!=EXITING){
-		// handle other states
-		if(rc_get_state()==RUNNING){
-			// do things
+		
+		//Get raw wheel positions from encoders
+		int rightpos=rc_get_encoder_pos(rightchannel);
+		int leftpos=rc_get_encoder_pos(leftchannel);
+		
+		
+		//Convert raw positions to radians
+		float rightrads=2*3.14159265358979323*rightpos/gearboxratio;
+		float leftrads=2*3.14159265358979323*leftpos/gearboxratio;	
+		
+		//update wheel postion display
+		printf("\r");
+		printf("%6f  |", rightrads);
+		printf("%6f  |", leftrads);
+		fflush(stdout);
+		
+		//If stay at reference mode enabled
+		if(modeflag==0){
+
+			if(rightpos>0){
+				if(rc_set_motor(rightchannel, rightpolarity*k*rightrads))
+				{
+					fprintf(stderr,"RIGHT MOTOR ERROR: Shutting down!\n");
+					return -1;
+				}
+				
+			}
+			if(rightpos<0){
+				if(rc_set_motor(rightchannel, rightpolarity*k*rightrads))
+				{
+					fprintf(stderr,"RIGHT MOTOR ERROR: Shutting down!\n");
+					return -1;
+				}
+				
+			}	
+			
 			rc_set_led(GREEN, ON);
 			rc_set_led(RED, OFF);
 		}
-		else if(rc_get_state()==PAUSED){
-			// do other things
+		else if(modeflag==1){
+		//else if(rc_get_state()==PAUSED){
+			// Follow the undriven wheel mode
+			
+			int rightpos=rc_get_encoder_pos(rightchannel);
+			int leftpos=rc_get_encoder_pos(leftchannel);	
+			
+			float rightrads=2*3.14159265358979323*rightpos/gearboxratio;
+			float leftrads=2*3.14159265358979323*leftpos/gearboxratio;		
+				
+			float posdiff=rightrads+leftrads;	
+				
+				
+			if((posdiff)>0)
+			{
+				if(rc_set_motor(rightchannel, rightpolarity*k*(posdiff)))
+				{
+					fprintf(stderr,"RIGHT MOTOR ERROR: Shutting down!\n");
+					return -1;
+				}
+				
+			}
+			if((posdiff)<0)
+			{
+				if(rc_set_motor(rightchannel, rightpolarity*k*(posdiff)))
+				{
+					fprintf(stderr,"RIGHT MOTOR ERROR: Shutting down!\n");
+					return -1;
+				}
+				
+			}
+			
 			rc_set_led(GREEN, OFF);
 			rc_set_led(RED, ON);
 		}
 		// always sleep at some point
-		usleep(100000);
+		usleep(10000);
 	}
 	
 	// exit cleanly
@@ -72,6 +209,8 @@ void on_pause_released(){
 	// toggle betewen paused and running modes
 	if(rc_get_state()==RUNNING)		rc_set_state(PAUSED);
 	else if(rc_get_state()==PAUSED)	rc_set_state(RUNNING);
+	if (modeflag==0) modeflag=1;
+	else if (modeflag==1) modeflag=0;
 	return;
 }
 
